f11dca6b6a0264fd1d53e40a73cb0d8bc8ab7869
WIN_NT\x16g025 <x16g025@chiba-fjb.ac.jp>
2017-12-01 10:02:30 +0900

    コミット

--- src/sabakan.java
+++ src/sabakan.java
@@ -1,16 +1,16 @@
+import java.util.Scanner;
 
 public class sabakan {
 
 	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("値：");
+		String c = s.next();
+		
 		for(int i = 0; i<=10; i++){
-			System.out.println("テストよろしく");
-			System.out.println("テストよろしく");
-			System.out.println("テストよろしく");
-			System.out.println("テストよろしく");
-			System.out.println("テストよろしく");
-			System.out.println("テストよろしく");
-			System.out.println("テストよろしく");
-			System.out.println("テストよろしく");
+			System.out.println(c + "よろしく");
 		}
 	}
 
