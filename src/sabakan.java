7b04c5d84cc111f46133d9364d99c14f7b244fb8
WIN_NT\x16g025 <x16g025@chiba-fjb.ac.jp>
2017-12-01 10:41:50 +0900

    scanner

--- src/sabakan.java
+++ src/sabakan.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 
 public class sabakan {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		for(int i = 0; i<=5; i++){
 			System.out.println("よろしく");
 		}
